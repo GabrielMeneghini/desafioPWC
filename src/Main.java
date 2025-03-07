f17275716077bdb4b37fc8e1488b532b5734218d
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2025-03-07 10:51:58 -0300

    Implementado case 5

--- src/Main.java
+++ src/Main.java
@@ -9,44 +9,52 @@ public class Main {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("*** Desafio PWC ***");
         menu();
     }
 
     public static void menu() {
         int escolha = -1;
         while(escolha != 0) {
-            System.out.println("""
-                \n*** Desafio PWC ***
-                
-                Digite um número para escolher um dos tipos de manipulação abaixo:
-                1- Reverta a ordem das palavras da string, mantendo a ordem das palavras.
+            System.out.print("""
+                \n1- Reverta a ordem das palavras da string, mantendo a ordem das palavras.
                 2- Remova todos os caracteres duplicados da string.
-                3- Encontre a substring palíndroma mais longa em uma frase.
+                3- Encontre a substring palíndroma mais longa em uma frase/palavra.
                 4- Coloque em maiúscula a primeira letra de cada frase em uma string.
                 5- Verifique se a string é um anagrama de um palíndromo.
+                0- Sair.
                 
-                0- Sair.""");
+                Digite um número para escolher um dos tipos de manipulação acima:""");
             escolha = sc.nextInt(); sc.nextLine();
             switch(escolha) {
                 case 1:
                     System.out.print("Digite a string a ser invertida: ");
                     String s1 = sc.nextLine();
-                    System.out.println(reverterOrdemPalavras(s1));
+                    System.out.println("Frase invertida: " + reverterOrdemPalavras(s1));
                     break;
                 case 2:
                     System.out.print("Digite a string a ser formatada: ");
                     String s2 = sc.nextLine();
-                    System.out.println(removerCaracteresDuplicados(s2));
+                    System.out.println("String sem caracteres duplicados: " + removerCaracteresDuplicados(s2));
                     break;
                 case 3:
                     System.out.print("Digite a string: ");
                     String s3 = sc.nextLine();
-                    System.out.println(maiorSubStringPalindroma(s3));
+                    System.out.println("Substring palíndroma mais longa encontrada: " + maiorSubStringPalindroma(s3));
                     break;
                 case 4:
                     System.out.print("Digite uma frase: ");
                     String s4 = sc.nextLine();
-                    System.out.println(primeiraLetraMaiuscula(s4));
+                    System.out.println("String formatada: " + primeiraLetraMaiuscula(s4));
+                    break;
+                case 5:
+                    System.out.print("Digite uma frase: ");
+                    String s5 = sc.nextLine();
+                    if(ehAnagramaDePalindromo(s5)) {
+                        System.out.println("\"" + s5 + "\" é um anagrama de um palíndromo.");
+                    } else {
+                        System.out.println("\"" + s5 + "\" não é um anagrama de um palíndromo.");
+                    }
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -61,7 +69,6 @@ public class Main {
     public static String reverterOrdemPalavras(String s) {
         List<String> stringList = stream(s.split(" ")).collect(Collectors.toList());
         Collections.reverse(stringList);
-        System.out.println("Frase invertida:");
         String stringInvertida = "";
         for(String x : stringList) {
             stringInvertida += x + " ";
@@ -99,6 +106,26 @@ public class Main {
         List<String> frases = stream(s.split("(?<=[.!?])\\s*")).toList();
 
         return frases.stream().map(x -> x.substring(0, 1).toUpperCase() + x.substring(1)).reduce("", (x1, x2)  -> x1 + " " + x2).trim();
+    }
+
+    public static boolean ehAnagramaDePalindromo(String s) {
+        s = normalizeString(s);
+
+        // Adicionando chaves e valores no Map
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+
+        // Contando chaves com valores impares
+        int totChavesImpares = 0;
+        for(Integer mapValue: map.values()) {
+            if(mapValue%2 != 0) {
+                totChavesImpares++;
+            }
+        }
+
+        return (totChavesImpares <= 1);
     }
 
     public static boolean ehPalindromo(String palavra) {
